77cb03519bf77300f851b8f96434cfa2acffbbab
Shuchir Jain <53063247+CoolCoderSJ@users.noreply.github.com>
2022-08-17 10:01:58 -0400

    finish

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/projects")
+def projects():
+    data = json.loads(open("data/projects.json").read())
+    return render_template("projects.html", data=data)
+
+app.run(port=5001, debug=True)
