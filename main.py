88dbce0dbcf82f563c813504b47606a53df0ff3e
Shuchir Jain <53063247+CoolCoderSJ@users.noreply.github.com>
2023-05-27 00:43:14 +0000

    updates

--- main.py
+++ main.py
@@ -11,9 +11,19 @@ def index():
 def extras():
     return render_template("guides.html")
 
+@app.route("/desecurly")
+def desecurly():
+    return render_template("desecurly.html")
+
 @app.route("/projects")
 def projects():
     data = json.loads(open("data/projects.json").read())
     return render_template("projects.html", data=data)
 
-app.run(port=5001, debug=True)
+@app.route("/resume")
+def resume():
+    return render_template("resume.html")
+
+
+
+app.run(host='0.0.0.0', port=5001, debug=True)
