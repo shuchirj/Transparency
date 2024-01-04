37055011208b86e867499e83ad5a70b675217fff
shuchir.jain <shuchir.jain@gmail.com>
2024-01-04 03:57:56 +0000

    redesign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, redirect, flash
 import json
-from discord_webhook import DiscordWebhook
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -18,11 +17,6 @@ def index():
             msg = request.args.get("msg")
         else: msg = None
         return render_template("index.html", msg=msg)
-    if request.method == "POST":
-        form = request.form
-        webhook = DiscordWebhook(url='https://discord.com/api/webhooks/1129960363601965056/sAxVLp_SOe74zsj3eQ6IfWz5LToc3y2IaXU_tfNlmdHeY_DsqU5hXqPEulpvGK5FqSYB', content=f"""Name: {form['firstname']} {form['lastname']}\nEmail: {form['email']}\nPhone Number: {form['phonenumber']}\nMessage: {form['details']}""")
-        response = webhook.execute()
-        return redirect("/?msg=Your message has been sent!")
 
 @app.route("/request", methods=['GET', 'POST'])
 def requestwork():
