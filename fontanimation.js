5880b7f4ef466328bf7d4ebb48bb44f55a43a336
Shuchir-Jain <shuchir.jain@gmail.com>
2024-08-14 00:37:59 -0400

    start portfolio rewrite

---
+++ fontanimation.js
@@ -0,0 +1,19 @@
+var textWrapper = document.querySelectorAll('.ml12');
+
+textWrapper.forEach(function(el) {
+    el.innerHTML = el.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+    anime.timeline({loop: false})
+    .add({
+        targets: '.ml12 .letter',
+        translateX: [40,0],
+        translateZ: 0,
+        opacity: [0,1],
+        easing: "easeOutExpo",
+        duration: 1500,
+        delay: (el, i) => 500 + 100 * i
+    })
+})
+
+
+setTimeout(() => document.querySelector("#page").style.opacity = "1", 100);
